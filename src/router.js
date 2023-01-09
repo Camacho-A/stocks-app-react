0c94f502dd0f3ac9fbd48297ac72a6a4e26707fb
Angelica Camacho <113385869+Camacho-A@users.noreply.github.com>
2023-01-08 20:14:25 -0500

    create components and pages

---
+++ src/router.js
@@ -0,0 +1,40 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+} from "react-router-dom";
+import App from './App';
+// import Main from './pages/main'
+// import Stocks from './pages/stocks';
+// import About from './pages/about';
+import { stocksLoader } from "./loaders";
+import Show from "./pages/show";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route 
+            path="/" 
+            element={<App/>}
+        >
+                <Route
+                    path=''
+                    element={<Main/>}
+                />
+                <Route 
+                    path='stocks'
+                    element={<Stocks/>}
+                />
+                <Route 
+                    path='about'
+                    element={<About/>}
+                />
+                <Route 
+                    path='stocks/:symbol'
+                    loader={stocksLoader}
+                    element={<Show/>}
+                />
+        </Route>
+    )
+);
+
+export default router;
